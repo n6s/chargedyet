3875264827b0f1748719a5a87e30cb86a2ca6d13
Roger Brown <roger.brown@perfectsearchcorp.com>
2017-12-27 11:24:33 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# chargedyet
-App written in Go to display a notification every 30 minutes reminding me to check if my device has finished charging yet.
+# schoolbell
+App written in Go. Display a notification every 60 minutes reminding me to begin studying the next topic in my study plan, much like school periods.
--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 var notify *notificator.Notificator
 
 func main() {
-	title := "Charged?"
-	message := "Has it finished charging yet?"
-	iconpath := "/home/rbrown/Pictures/charging.png"
+	title := "schoolbell"
+	message := "Switch to next topic in study plan."
+	iconpath := "/home/rbrown/Pictures/amar-School-Bell-300px.png"
 	for {
 		notifyMe(title, message, iconpath)
 
-		timer1 := time.NewTimer(time.Minute * 30)
+		timer1 := time.NewTimer(time.Minute * 60)
 		<-timer1.C
 	}
 }
